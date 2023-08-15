8b2eb87c9e44afa9f11c916d6074c600ba50df7c
Aurelio Borges <aurelioaborges@gmail.com>
2023-08-15 15:41:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const inputItem = document.getElementById('input-item')
+const addButton = document.getElementById('add-button')
+
+const createNewItem = function (newItem) {
+  const listItem = document.createElement('li')
+  const checkBox = document.createElement('input')
+  const itemText = document.createElement('p')
+  const deleteButton = document.createElement('button')
+
+
+}
